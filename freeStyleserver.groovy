94185854fb364b413796c2c4d8191a1ed898129f
Amit Kumar Pandey <pandeyak1991@gmail.com>
2024-03-26 18:05:06 +0530

    typo

--- freeStyleserver.groovy
+++ freeStyleserver.groovy
@@ -118,7 +118,7 @@ freeStyleJob("${FolderName}/server"){
                     //     }
                     // }
 
-                    }
+                    // }
                 }
             }
         }
